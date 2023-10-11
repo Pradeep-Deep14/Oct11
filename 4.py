7e8b3700d2ca0f5d983658b5d29021fd6acae51f
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-11 18:49:08 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,14 @@
+def change_list(lst):
+    arr[-1]='modified'
+    return arr 
+
+my_list=['original','values']
+print(change_list(my_list))
+
+
+#def change_list(lst):
+#    lst[-1] = 'modified'
+#    return lst
+
+#my_list = ['original', 'values']
+#print(change_list(my_list))
