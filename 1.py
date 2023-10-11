158743be641dbd5634015b894f314d095d518271
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-11 18:43:11 +0530

    first commit

--- 1.py
+++ 1.py
@@ -2,3 +2,4 @@ set1={1,1,2,3,3}
 set2=set1.copy()
 set2.add(4)
 print(set1)
+#Set is a collection of unique elements
---
+++ 2.py
@@ -0,0 +1,9 @@
+set1={1,2,3}
+set2={4,5,6}
+print(len(set1+set2))
+
+
+#set1 = {1, 2, 3}
+#set2 = {4, 5, 6}
+#union_set = set1 | set2  # or union_set = set1.union(set2)
+#print(len(union_set))
